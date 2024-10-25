c26d82bd5f60104953383e338e670a2232419dd0
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-10-25 22:00:29 +0530

    State and conditional content

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,11 +2,13 @@ import {Post} from "./Post.jsx";
 import styles from "./PostsList.module.css";
 import NewPost from "./NewPost.jsx";
 import {useState} from "react";
+import {Modal} from "./Modal.jsx";
 
 export const PostsList = () => {
 
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
+  const [modalIsVisible, setModalIsVisible] = useState(true);
 
   function onBodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -16,9 +18,17 @@ export const PostsList = () => {
     setEnteredAuthor(event.target.value);
   }
 
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
     return (
       <>
-        <NewPost onBodyChange={onBodyChangeHandler} onAuthorChange={onAuthorChangeHandler} />
+      { modalIsVisible &&
+        <Modal onClose={hideModalHandler} >
+          <NewPost onBodyChange={onBodyChangeHandler} onAuthorChange={onAuthorChangeHandler} />
+        </Modal>
+      }
         <ul className={styles.posts}>
           <Post author={enteredAuthor} body={enteredBody}/>
           <Post author='Nikhil' body='Nikhil"s first book'/>
