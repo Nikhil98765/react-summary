ac57c20350372fb602544ad21ed4f294dc5d2a72
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-10-25 23:45:34 +0530

    Added shared header and more state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import {PostsList} from "./components/PostsList.jsx";
+import { useState } from "react";
+
+import { PostsList } from "./components/PostsList.jsx";
+import MainHeader from "./components/MainHeader.jsx";
 
 function App() {
+
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
   return (
-    <main>
-      <PostsList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler}></MainHeader>
+      <main>
+        <PostsList isPosting={modalIsVisible} onStopPosting={hideModalHandler}/>
+      </main>
+    </>
   );
 }
 
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,11 +4,11 @@ import NewPost from "./NewPost.jsx";
 import {useState} from "react";
 import {Modal} from "./Modal.jsx";
 
-export const PostsList = () => {
+export const PostsList = ({isPosting, onStopPosting}) => {
 
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
-  const [modalIsVisible, setModalIsVisible] = useState(true);
+ 
 
   function onBodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -18,22 +18,21 @@ export const PostsList = () => {
     setEnteredAuthor(event.target.value);
   }
 
-  function hideModalHandler() {
-    setModalIsVisible(false);
-  }
 
     return (
       <>
-      { modalIsVisible &&
-        <Modal onClose={hideModalHandler} >
-          <NewPost onBodyChange={onBodyChangeHandler} onAuthorChange={onAuthorChangeHandler} />
-        </Modal>
-      }
+        {isPosting && (
+          <Modal onClose={onStopPosting}>
+            <NewPost
+              onBodyChange={onBodyChangeHandler}
+              onAuthorChange={onAuthorChangeHandler}
+            />
+          </Modal>
+        )}
         <ul className={styles.posts}>
-          <Post author={enteredAuthor} body={enteredBody}/>
-          <Post author='Nikhil' body='Nikhil"s first book'/>
+          <Post author={enteredAuthor} body={enteredBody} />
+          <Post author="Nikhil" body='Nikhil"s first book' />
         </ul>
       </>
-
-    )
+    );
 }
