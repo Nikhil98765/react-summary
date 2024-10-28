a10b5f768ded6627562ca613acb54e9f2253b2de
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-10-28 13:16:58 +0530

    Using useEffect to handle API requests and handled loading state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 import { Post } from "./Post.jsx";
 import styles from "./PostsList.module.css";
@@ -8,6 +8,18 @@ import {Modal} from "./Modal.jsx";
 export const PostsList = ({isPosting, onStopPosting}) => {
 
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     fetch('http://localhost:8080/posts', {
@@ -30,7 +42,7 @@ export const PostsList = ({isPosting, onStopPosting}) => {
             />
           </Modal>
         )}
-        {
+        { !isFetching && (
           posts.length > 0 ?
             <ul className={styles.posts}>
               {posts.map((post, index) => (
@@ -42,7 +54,15 @@ export const PostsList = ({isPosting, onStopPosting}) => {
               <h2>There are no posts yet.</h2>
               <p>Start adding some!</p>
             </div>
+          )
         }
+        {
+          isFetching &&
+          <div style={{textAlign: 'center', color: 'white'}}>
+            <p>Loading Posts...</p>
+          </div>
+        }
+
       </>
     );
 }
